0a5298e191228da4cd14156c8e4a43aa0137bd6e
bostero854 <bostero854@gmail.com>
2021-06-18 17:35:17 -0300

    Cambio Nombre

--- WebApplication1/Index.aspx.cs
+++ WebApplication1/Index.aspx.cs
@@ -1,19 +1,12 @@
-﻿using Entidades;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System;
 
 namespace WebApplication1
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class Index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             Negocio.Conectado conectado = new Negocio.Conectado();
-
-
             //Muestro los datos en la Grilla.
             GridView1.DataSource = conectado.Mododesconectado();
             GridView1.DataBind();
@@ -22,10 +15,5 @@ namespace WebApplication1
             GridView2.DataSource = conectado.ModoConectado();
             GridView2.DataBind();
         }
-
-        //Le paso a la grilla la Lista generada con los campos.
-        //GridView1.DataSource = lstU;
-        ////Muestro los datos en la Grilla.
-        //GridView1.DataBind();
     }
 }
