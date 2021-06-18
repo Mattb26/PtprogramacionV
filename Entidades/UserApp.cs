42a951794d0ec80479fb89e715515ff40541e03e
bostero854 <bostero854@gmail.com>
2021-06-18 00:14:07 -0300

    inicio

---
+++ Entidades/UserApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class UserApp
+    {
+
+         public int Id { get; set; }
+         public string UserName { get; set; }
+         public string LastName { get; set; }
+         public DateTime BirthDate { get; set; }
+
+    }
+}
